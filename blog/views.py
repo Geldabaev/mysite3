19894ac7fccbb7a68be62630c8cd27338931c77d
yunus <geldabaev@mail.ru>
2023-10-21 09:45:46 +0300

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+
+def index(request):
+    return render(request, "blog/index.html", {})
+
+
+def category(request, catid):
+    return HttpResponse(f"<h1>Cats{catid}</h1>")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>Странциа не найдена</h1>")
+
